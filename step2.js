ac89ab8177bc56aecbaca876fb4c57ae453c1663
Hatice Kacer <haticemim17@gmail.com>
2022-12-24 22:59:52 -0600

    changes

--- step2.js
+++ step2.js
@@ -1,3 +1,5 @@
+//runtime complexity= O(n^2)
+//space Complexity= O(1)
 function addToZero(nums) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
@@ -8,7 +10,20 @@ function addToZero(nums) {
   }
   return false;
 }
+addToZero([]);
+// -> False
 
+addToZero([1]);
+// -> False
+
+addToZero([1, 2, 3]);
+// -> False
+
+addToZero([1, 2, 3, -2]);
+// -> True
+
+//runtime Complexity= O(n^2)
+//space Complexity= O(1)
 function hasUniqueCharacters(word) {
   for (let i = 0; i < word.length; i++) {
     for (let j = i + 1; j < word.length; j++) {
@@ -20,7 +35,15 @@ function hasUniqueCharacters(word) {
   return true;
 }
 console.log(hasUniqueCharacters('hello')); // false
+hasUniqueCharacters("Monday");
+// -> True
 
+hasUniqueCharacters("Moonday");
+// -> False
+
+
+//runtime Complexity= O(n)
+//space Complexity= O(1)
 function isPangram(sentence) {
   // Convert the sentence to lowercase and remove all non-letter characters
   const cleanedSentence = sentence.toLowerCase().replace(/[^a-z]/g, '');
@@ -30,18 +53,20 @@ function isPangram(sentence) {
   for (let i = 0; i < cleanedSentence.length; i++) {
     seenLetters[cleanedSentence.charCodeAt(i) - 97] = true;
   }
-
   // Check if all letters have been seen
   for (let i = 0; i < 26; i++) {
     if (!seenLetters[i]) {
       return false;
     }
   }
-
   return true;
 }
 console.log(isPangram('The quick brown fox jumps over the lazy dog.')); // true
 
+
+
+//runtime Complexity= O(n)
+//space Complexity= O(1)
 function findLongestWord(words) {
   let longestLength = 0;
   for (let i = 0; i < words.length; i++) {
